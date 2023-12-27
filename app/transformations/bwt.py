21e4e1dc26adabcbbe6b0d922ed4ac53f10609ff
sentenzo <sentenzo@users.noreply.github.com>
2023-12-27 14:03:26 +0300

    fixing a nasty bug

--- app/transformations/bwt.py
+++ app/transformations/bwt.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+from functools import cmp_to_key
+
 from .transform import Transformation
 
 ORIGIN_PTR_SIZE = 4
@@ -8,19 +12,37 @@ class BWT(Transformation):
         block_size = len(block)
         rotations = list(range(block_size))
 
+        class ComparableRotation:
+            def __init__(self, shift) -> None:
+                self.shift = shift
+
+            def __getitem__(self, index):
+                return block[(index + self.shift) % block_size]
+
+            def __lt__(self, other: ComparableRotation):
+                for i in range(block_size):
+                    if self[i] < other[i]:
+                        return True
+                    elif self[i] > other[i]:
+                        return False
+                    elif self[i] == other[i]:
+                        continue
+                return False
+
         def slice_key(rot):
-            return block[rot - block_size : rot]
+            return ComparableRotation(rot)
 
         rotations.sort(key=slice_key)
+
         origin_ptr = rotations.index(0)
-        origin_ptr_bytes = origin_ptr.to_bytes(ORIGIN_PTR_SIZE)
+        origin_ptr_bytes = origin_ptr.to_bytes(ORIGIN_PTR_SIZE, byteorder="big")
         encoded = bytearray(origin_ptr_bytes)
         for rot in rotations:
             encoded.append(block[rot - 1])
         return bytes(encoded)
 
     def decode(self, block: bytes) -> bytes:
-        origin_ptr = int.from_bytes(block[:ORIGIN_PTR_SIZE])
+        origin_ptr = int.from_bytes(block[:ORIGIN_PTR_SIZE], byteorder="big")
         block = block[ORIGIN_PTR_SIZE:]
         block_size = len(block)
         decoded = [None] * block_size
