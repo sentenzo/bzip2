517b2e6f1df78381cbb1c1f5007ec2377b02d672
sentenzo <sentenzo@users.noreply.github.com>
2023-12-28 11:05:12 +0300

    refactoring

---
+++ app/bzip2.py
@@ -0,0 +1,14 @@
+from packager import Packager
+from transformations import BWT, HFC, MTF, RLE
+
+bzip2 = RLE() >> BWT() >> MTF() >> RLE() >> HFC()
+
+
+def encode(in_file, out_file):
+    packager = Packager(bzip2)
+    packager.apply_encoding(in_file, out_file)
+
+
+def decode(in_file, out_file):
+    packager = Packager(bzip2)
+    packager.apply_decoding(in_file, out_file)
--- app/main.py
+++ app/main.py
@@ -1,20 +1,17 @@
-from packager import Packager
-from transformations import BWT, HFC, MTF, RLE
+import filecmp
 
-# BLOCK_SIZE_BYTES = 1024 * 512  # 0.5 Mib
-BLOCK_SIZE_BYTES = 1024 * 64  # 0.5 Mib
-
+from bzip2 import decode, encode
 
 if __name__ == "__main__":
-    in_file = r"./test_file.txt"
-    enc_file = r"./test_file.sbzip"
-    dec_file = r"./test_file.decoded.txt"
+    in_file = r"./Tolstoy Leo - War and Peace.txt"
+    enc_file = in_file + ".enc"
+    dec_file = enc_file + ".dec"
 
-    sbzip = RLE() >> BWT() >> MTF() >> RLE() >> HFC()
-    packager = Packager(sbzip)
+    encode(in_file, enc_file)
+    decode(enc_file, dec_file)
 
-    packager.apply_encoding(in_file, enc_file)
-    packager.apply_decoding(enc_file, dec_file)
+    # checking if the files are identical
+    assert filecmp.cmp(in_file, dec_file, shallow=False)
 
     # import cProfile
     # cProfile.run("transform(in_file , enc_file, sbzip.encode)")
