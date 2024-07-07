fbacece692433b4e620fa90a7a8f47b839e2d7a8
sentenzo <sentenzo@users.noreply.github.com>
2024-07-07 10:51:45 +0300

    `BitArray` custom `byte_size` + LOADS of tests

--- app/transformations/hfc/bits.py
+++ app/transformations/hfc/bits.py
@@ -1,26 +1,36 @@
 from collections import deque
 
-BYTE_SIZE = 8
+REAL_BYTE_SIZE = 8
+DEFAULT_BYTE_SIZE = REAL_BYTE_SIZE
 
 
 class BitArray:
     @staticmethod
-    def gen_by_bits(byte, drop_lz=False):
-        for pos in range(BYTE_SIZE - 1, -1, -1):
+    def gen_by_bits(
+        byte,
+        drop_lz: bool = False,
+        byte_size: int = DEFAULT_BYTE_SIZE,
+    ):
+        for pos in range(byte_size - 1, -1, -1):
             bit = int(byte & (1 << pos) != 0)
             if bit:
                 drop_lz = False
             if not drop_lz:
                 yield bit
-        if drop_lz == True:
+        if drop_lz:
             yield 0
 
     @staticmethod
-    def gen_bit_stream(chunk_of_bytes, *, drop_last=0):
-        total_bit_count = len(chunk_of_bytes) * BYTE_SIZE
+    def gen_bit_stream(
+        chunk_of_bytes: list[int],
+        *,
+        drop_last=0,
+        byte_size: int = DEFAULT_BYTE_SIZE,
+    ):
+        total_bit_count = len(chunk_of_bytes) * byte_size
         processed_bit_count = 0
         for byte in chunk_of_bytes:
-            for bit in BitArray.gen_by_bits(byte):
+            for bit in BitArray.gen_by_bits(byte, byte_size=byte_size):
                 yield bit
                 processed_bit_count += 1
                 if processed_bit_count == total_bit_count - drop_last:
@@ -28,42 +38,41 @@ class BitArray:
             if processed_bit_count == total_bit_count - drop_last:
                 break
 
-    # @staticmethod
-    # def from_bit_iterable(iter):
-    #     bit_array = BitArray()
-    #     for b in iter:
-    #         bit_array.append_bit(int(b))
-    #     return bit_array
+    def __init__(self, byte_size: int = DEFAULT_BYTE_SIZE) -> None:
+        self.byte_size = byte_size
+        self.bytes: list[int] = []
+        self.acc_bits: deque[int] = deque()
 
     def flush_bits(self):
-        while len(self.acc_bits) >= 8:
+        while len(self.acc_bits) >= self.byte_size:
             byte = 0
-            for _ in range(8):
+            for _ in range(self.byte_size):
                 byte <<= 1
                 byte += self.acc_bits.popleft()
-            byte = byte.to_bytes(1, byteorder="big")[0]
+            # byte = byte.to_bytes(1, byteorder="big")[0]
             self.bytes.append(byte)
 
-    def __init__(self) -> None:
-        self.bytes = []
-        self.acc_bits = deque()
-
-    def append_bit(self, bit):
+    def append_bit(self, bit: int):
         self.acc_bits.append(bit)
         self.flush_bits()
 
-    def extend_bit(self, bits):
+    def extend_bit(self, bits: list[int | bool]):
         for bit in bits:
             self.acc_bits.append(int(bit))
         self.flush_bits()
 
-    def append_byte(self, byte):
-        for bit in BitArray.gen_by_bits(byte):
+    def append_byte(self, byte: int):
+        for bit in BitArray.gen_by_bits(
+            byte,
+            byte_size=self.byte_size,
+        ):
             self.acc_bits.append(bit)
         self.flush_bits()
 
-    def append_code(self, code):
-        for bit in BitArray.gen_by_bits(code, drop_lz=True):
+    def append_code(self, code: int):
+        for bit in BitArray.gen_by_bits(
+            code, drop_lz=True, byte_size=self.byte_size
+        ):
             self.acc_bits.append(bit)
         self.flush_bits()
 
@@ -71,7 +80,9 @@ class BitArray:
         byte_array = self.bytes[:]
         if self.acc_bits:
             last_byte = 0
-            for i in range(8):  # [01011] => [01011000] (filing with zeros)
+            for i in range(
+                self.byte_size
+            ):  # [01011] => [01011000] (filing with zeros)
                 last_byte <<= 1
                 if i < len(self.acc_bits):
                     last_byte += self.acc_bits[i]
@@ -79,7 +90,8 @@ class BitArray:
         return byte_array
 
     def __len__(self):
-        return len(self.bytes) * BYTE_SIZE + len(self.acc_bits)
+        return len(self.bytes) * DEFAULT_BYTE_SIZE + len(self.acc_bits)
 
     def __iter__(self):
-        ...
+        yield from self.gen_bit_stream(self.bytes, byte_size=self.byte_size)
+        yield from self.acc_bits
--- tests/unit/test_bits.py
+++ tests/unit/test_bits.py
@@ -4,17 +4,102 @@ from app.transformations.hfc.bits import BitArray
 
 
 @pytest.mark.parametrize(
-    "byte_str",
-    ["00000000", "00000001", "11111111", "00010101"],
+    "byte_size, byte_str_arr",
+    [
+        (8, ["00000000", "00000001", "11111111", "00010101"]),
+        (4, ["0000", "0001", "1111", "0110", "1010", "0011"]),
+        (7, ["0000000", "0000001", "1111111", "1101000", "1001011"]),
+        (13, ["0100101110110", "1100110100111", "1000110110000"]),
+    ],
 )
-def test_gen_by_bits(byte_str):
-    assert len(byte_str) == 8
-    byte_str_drop_lz = bin(int(byte_str, 2))[2:]  # "00101" => "101"
+def test_gen_by_bits(byte_size: int, byte_str_arr: list[str]):
+    for byte_str in byte_str_arr:
+        assert len(byte_str) == byte_size
+        byte_str_drop_lz = bin(int(byte_str, 2))[2:]  # "00101" => "101"
 
-    def reassemble(byte_str, drop_lz):
-        byte = int(byte_str, 2)
-        bit_list = list(BitArray.gen_by_bits(byte, drop_lz))
+        def reassemble(
+            byte_str,
+            drop_lz,
+            byte_size,
+        ):
+            byte = int(byte_str, 2)
+            bit_list = list(BitArray.gen_by_bits(byte, drop_lz, byte_size))
+            return "".join(map(str, bit_list))
+
+        assert reassemble(byte_str, False, byte_size) == byte_str
+        assert reassemble(byte_str, True, byte_size) == byte_str_drop_lz
+
+
+@pytest.mark.parametrize(
+    "byte_size, byte_str_arr",
+    [
+        (8, ["00000000", "00000001", "11111111", "00010101"]),
+        (4, ["0000", "0001", "1111", "0110", "1010", "0011"]),
+        (7, ["0000000", "0000001", "1111111", "1101000", "1001011"]),
+        (13, ["0100101110110", "1100110100111", "1000110110000"]),
+    ],
+)
+def test_gen_bit_stream(byte_size: int, byte_str_arr: list[str]):
+    byte_arr = []
+    for byte_str in byte_str_arr:
+        assert len(byte_str) == byte_size
+        byte_arr.append(int(byte_str, 2))
+
+    def get_received(drop_last) -> str:
+        bit_list = list(
+            BitArray.gen_bit_stream(
+                byte_arr, drop_last=drop_last, byte_size=byte_size
+            )
+        )
         return "".join(map(str, bit_list))
 
-    assert reassemble(byte_str, False) == byte_str
-    assert reassemble(byte_str, True) == byte_str_drop_lz
+    expected = "".join(byte_str_arr)
+    for drop_last in range(4):
+        received = get_received(drop_last=drop_last)
+        assert expected[: len(expected) - drop_last] == received
+
+
+@pytest.mark.parametrize(
+    "byte_size",
+    [3, 4, 7, 8, 13],
+)
+@pytest.mark.parametrize(
+    "bits_str",
+    ["000", "1111111111", "01001011101101111111000101011"],
+)
+def test_append_bit(byte_size, bits_str):
+    bits = list(map(int, bits_str))
+
+    barr = BitArray(byte_size)
+    for bit in bits:
+        barr.append_bit(bit)
+    received = "".join(str(bit) for bit in barr)
+    assert bits_str == received
+
+    barr = BitArray(byte_size)
+    barr.extend_bit(bits)
+    received = "".join(str(bit) for bit in barr)
+    assert bits_str == received
+
+
+@pytest.mark.parametrize(
+    "byte_size, byte_strs",
+    [
+        (3, ["110", "000", "001", "011"]),
+        (4, ["0110", "0000", "1001", "0111"]),
+        (8, ["01100000", "10010111"]),
+        (13, ["1100110100111", "0100101110110", "1000110110000"]),
+    ],
+)
+def test_append_byte(byte_size, byte_strs):
+    bytes_ = []
+    for byte_str in byte_strs:
+        assert len(byte_str) == byte_size
+        bytes_.append(int(byte_str, 2))
+
+    barr = BitArray(byte_size)
+    for byte in bytes_:
+        barr.append_byte(byte)
+
+    assert "".join(byte_strs) == "".join(map(str, barr))
+    assert bytes_ == barr.to_byte_array()
